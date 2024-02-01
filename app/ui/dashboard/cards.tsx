2b80bff52e945e860b73a7c87a9ecacd613c2906
NatureDev <naturesky0411@gmail.com>
2024-02-01 04:32:48 +0000

    12.mutating data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,31 +1,26 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import {BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon} from '@heroicons/react/24/outline';
+import {lusitana} from '@/app/ui/fonts';
+import {fetchCardData} from '@/app/lib/data';
 
 const iconMap = {
-  collected: BanknotesIcon,
-  customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+    collected: BanknotesIcon,
+    customers: UserGroupIcon,
+    pending: ClockIcon,
+    invoices: InboxIcon
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+    const {numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
+    return (<>
+     {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
-        type="customers"
-      /> */}
+        type="customers" /> 
     </>
   );
 }
@@ -44,15 +39,12 @@ export function Card({
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
-      </div>
-      <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-      >
-        {value}
-      </p>
-    </div>
+        {Icon ? <Icon className="h-5 w-5 text-gray-700" / > : null} < h3 className = "ml-2 text-sm font-medium" > {
+    title
+} </h3>
+      </div > <p
+    className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}>
+    {value}
+</p> </div>
   );
 }
